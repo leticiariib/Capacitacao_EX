03ec5ab0b03fbea6313b2799d094ea41e89c35a0
leticiariib <d2024010070@unifei.edu.br>
2024-06-07 11:48:08 -0300

    Add files via upload

---
+++ exercicio6.js
@@ -0,0 +1,11 @@
+//Realize diversas operações aritméticas utilizando os operadores vistos na capacitação.
+
+let a = 4; 
+
+m = a *= 1; 
+s = a += 1;
+d = a - 1;
+r = a % 2; 
+
+console.log(m, s, d, r);
+
