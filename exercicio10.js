bc860b625f88ef19af07c934b36d6a41d7012173
leticiariib <d2024010070@unifei.edu.br>
2024-06-13 20:44:54 -0300

    Delete exercicio10.js

--- exercicio10.js
+++
@@ -1,6 +0,0 @@
-//Crie um condição para verificar se o Ex é o melhor projeto da unifei
-//Use a váriavel abaixo:
-
-const ExMelhorProjeto = true;
-
-console.log(ExMelhorProjeto ? "o Ex e o melhor projeto da unifei!" : "o Ex nao e o melhor projeto da unifei!");
